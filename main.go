e05043eee22d10d5650910eda316d651bd04abd9
Loris <loris4526@gmail.com>
2018-08-09 08:26:15 +0200

    port from env variables

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/L-oris/herokuDeploy/controller"
 	"github.com/julienschmidt/httprouter"
@@ -17,11 +18,20 @@ func main() {
 	router.POST("/", controller.AddToMessage)
 	router.DELETE("/", controller.ResetMessage)
 
-	port := ":80"
+	port := determineEnvPort()
 	log.Println("listen on port", port)
-	err := http.ListenAndServe(port, router)
-	if err != nil {
+	if err := http.ListenAndServe(port, router); err != nil {
 		fmt.Println("error listening to port", port, ":", err)
 		return
 	}
+}
+
+func determineEnvPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Println("PORT not found in env variable, using default setting to :80")
+		port = ":8080"
+	}
+
+	return port
 }
